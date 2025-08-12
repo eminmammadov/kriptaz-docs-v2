8038367bcb2080b6ada8e2339802753733f293d6
Emin Mammadov <eminmammadovcom@gmail.com>
2025-08-12 14:57:23 +0400

    menu-updates

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -47,15 +47,199 @@ export const menuData: MenuItem[] = [
 
   {
     id: 'token-ekonomiyasi',
-    title: 'Token Ekonomiyası',
+    title: 'Token ekonomiyası',
     href: '/komek/token-ekonomiyasi',
     hasSeparator: true
   },
   {
     id: 'yol-xeritemiz',
-    title: 'Yol Xəritəmiz',
+    title: 'Yol xəritəmiz',
     href: '/komek/yol-xeritemiz',
   },  
+
+  
+  {
+    id: 'k-media',
+    title: 'Kriptaz Media',
+    isExpandable: true,
+    isExpanded: false,
+    hasSeparator: true,
+    children: [
+      {
+        id: 'giris',
+        title: 'Giriş',
+        href: '/k-media/giriş'
+      },
+      {
+        id: 'yq-xebeler',
+        title: 'Y/Q xəbərlər',
+        href: '/k-media/yq-xebeler'
+      },
+      {
+        id: 'airdrops',
+        title: 'Airdrops',
+        href: '/k-media/airdrops'
+      },
+      {
+        id: 'sohub',
+        title: 'SoHub',
+        href: '/k-media/sohub'
+      },
+    ]
+  },
+  
+  {
+    id: 'kriptaz-x',
+    title: 'KriptazX',
+    isExpandable: true,
+    isExpanded: false,
+    children: [
+      {
+        id: 'giris',
+        title: 'Giriş',
+        href: '/kriptaz-x/giriş'
+      },
+      {
+        id: 'smart-wall',
+        title: 'SmartWall',
+        href: '/kriptaz-x/smart-wall'
+      },
+      {
+        id: 'strategy',
+        title: 'Strategy',
+        href: '/kriptaz-x/strategy'
+      },
+      {
+        id: 'web3-dev',
+        title: 'Web3 Dev.',
+        href: '/kriptaz-x/web3-dev'
+      },
+      {
+        id: 'data-visualization',
+        title: 'Data visualization',
+        href: '/kriptaz-x/data-visualization'
+      },
+    ]
+  },
+
+  {
+    id: 'kriptaz-pay',
+    title: 'Kriptaz Pay',
+    isExpandable: true,
+    isExpanded: false,
+    children: [
+      {
+        id: 'giris',
+        title: 'Giriş',
+        href: '/kriptaz-pay/giriş'
+      },
+      {
+        id: 'gateway-api',
+        title: 'Gateway API',
+        href: '/kriptaz-pay/gateway-api'
+      },
+      {
+        id: 'texnologiya',
+        title: 'Texnologiya',
+        href: '/kriptaz-pay/texnologiya'
+      },
+      {
+        id: 'hd',
+        title: 'Hierarchical Deterministic',
+        href: '/kriptaz-pay/hd'
+      },
+      {
+        id: 'on-off-chain',
+        title: 'On/Off-Chain',
+        href: '/kriptaz-pay/on-off-chain'
+      },
+      {
+        id: 'p2p',
+        title: 'P2P',
+        href: '/kriptaz-pay/p2p'
+      },
+      {
+        id: 'paypoint',
+        title: 'Paypointlər',
+        href: '/kriptaz-pay/paypoint'
+      },
+
+      {
+        id: 'kxhm',
+        title: 'Kriptaz Card (KXHM)',
+        href: '/kriptaz-qanunlar/kxhm'
+      },
+      {
+        id: 'kaiem',
+        title: 'KAİE müqaviləsi',
+        href: '/kriptaz-qanunlar/kaiem'
+      },
+    ]
+  },
+  
+  {
+    id: 'kriptaz-edu',
+    title: 'Kriptaz Edu',
+    isExpandable: true,
+    isExpanded: false,
+    children: [
+      {
+        id: 'giris',
+        title: 'Giriş',
+        href: '/kriptaz-edu/giriş'
+      },
+      {
+        id: 'video-dersler',
+        title: 'Video dərslər',
+        href: '/kriptaz-edu/video-dersler'
+      },
+      {
+        id: 'book-market',
+        title: 'Kitab mağazası',
+        href: '/kriptaz-edu/book-market'
+      },
+      
+    ]
+  },
+
+  {
+    id: 'kriptaz-invest-founders',
+    title: 'Kriptaz Invest Founders',
+    isExpandable: true,
+    isExpanded: false,
+    hasSeparator: true,
+    children: []
+  },
+
+  {
+    id: 'kriptaz-qanunlar',
+    title: 'Kriptaz qanunları',
+    isExpandable: true,
+    isExpanded: false,
+    hasSeparator: true,
+    children: [
+      {
+        id: 'kxhm',
+        title: 'KXHM-Kripto Xərcləmə Hesabı',
+        href: '/kriptaz-qanunlar/kxhm'
+      },
+      {
+        id: 'kaiem',
+        title: 'KAİEM-Kripto Aktivlərin İdarə Edilməsi',
+        href: '/kriptaz-qanunlar/kaiem'
+      },
+      {
+        id: 'xususi-tenzimlemeler',
+        title: 'Xüsusi tənzimləmələr',
+        href: '/kriptaz-qanunlar/xususi-tenzimlemeler'
+      },
+      {
+        id: 'musteri-munasibetleri',
+        title: 'Müştəri münasibətləri',
+        href: '/qanunlar/musteri-munasibetleri'
+      }
+    ]
+  },
 
   {
     id: 'komek',
@@ -65,7 +249,7 @@ export const menuData: MenuItem[] = [
     children: [
       {
         id: 'deyisiklikler-jurnali',
-        title: 'Dəyişikliklər Jurnalı',
+        title: 'Dəyişikliklər jurnalı',
         href: '/komek/deyisiklikler-jurnali'
       },
       {
@@ -75,109 +259,28 @@ export const menuData: MenuItem[] = [
       },
     ]
   },
-  
-  
-  
+
   {
-    id: '2x-grid',
-    title: '2x Grid',
-    href: '/2x-grid',
-    hasSeparator: true
-  },
-  
-  {
-    id: 'ibm-logos',
-    title: 'IBM logos',
+    id: 'reqemsal-qanunlar',
+    title: 'Rəqəmsal qanunlar',
     isExpandable: true,
     isExpanded: false,
     children: [
       {
-        id: '8-bar',
-        title: '8-Bar',
-        href: '/ibm-logos/8-bar'
+        id: 'mexfiglik-siyaseti',
+        title: 'Məxfilik siyasəti',
+        href: '/reqemsal-qanunlar/mexfiglik-siyaseti'
       },
       {
-        id: 'rebus',
-        title: 'Rebus',
-        href: '/ibm-logos/rebus'
+        id: 'istifade-qaydaları',
+        title: 'İstifadə qaydaları',
+        href: '/reqemsal-qanunlar/istifade-qaydaları'
+      },
+      {
+        id: 'kuki-siyaseti',
+        title: 'Kuki siyasəti',
+        href: '/reqemsal-qanunlar/kuki-siyaseti'
       }
     ]
   },
-  
-  {
-    id: 'iconography',
-    title: 'Iconography',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  },
-  
-  {
-    id: 'illustration',
-    title: 'Illustration',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  },
-  
-  {
-    id: 'photography',
-    title: 'Photography',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  },
-  
-  {
-    id: 'data-visualization',
-    title: 'Data visualization',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  },
-  
-  {
-    id: 'infographics',
-    title: 'Infographics',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  },
-  
-  {
-    id: 'layout',
-    title: 'Layout',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  },
-  
-  {
-    id: 'animation',
-    title: 'Animation',
-    isExpandable: true,
-    isExpanded: false,
-    children: [],
-    hasSeparator: true
-  },
-  
-  {
-    id: 'resources',
-    title: 'Resources',
-    href: '/resources'
-  },
-  
-  {
-    id: 'whats-new',
-    title: "What's new",
-    href: '/whats-new'
-  },
-  
-  {
-    id: 'help',
-    title: 'Help',
-    isExpandable: true,
-    isExpanded: false,
-    children: []
-  }
 ];
